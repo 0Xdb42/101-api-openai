15177275cad13be1b4deb0692ee9677ea9fba8ac
Đ <github@clonix.info>
2023-04-15 10:45:19 +0200

    init

---
+++ main.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import os
+import openai
+
+class OpenAI_BOT:
+    """ """
+    def __init__(self):
+        """ """
+        print("~[ Welcome to chatGPT 3.5 Turbo in CLI ]~")
+        self.api_key = os.getenv("OPENAI_API_KEY", None)
+
+
+curl https://api.openai.com/v1/images/generations \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $OPENAI_API_KEY" \
+  -d '{
+    "prompt": "a white siamese cat",
+    "n": 1,
+    "size": "1024x1024"
+  }'
+
+
+    def infinite_prompt(self):
+        """ """
+        history = []
+        system = {'role': 'system', 'content': 'you are a helpful assistant.'}
+        history.append(system)
+        while True:
+            prompt = input("> ")
+            if prompt == "exit":
+                break
+            message = {"role":"user", "content":prompt}
+            history.append(message)
+            try:
+                reply = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=history)
+            except openai.error.AuthenticationError:
+                print("~[ Erreur d'authentification ]~")
+                break
+            content = reply['choices'][0]['message']['content']
+            history.append({"role":"assistant", "content":content})
+            print(f"< {content}\n")
+        print("~[ Bye !]~")
+        return
+
+def main():
+    bot = OpenAI_BOT()
+    bot.infinite_prompt()
+
+if __name__ == "__main__":
+    main()
